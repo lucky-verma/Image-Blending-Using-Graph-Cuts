232069028d173ea1a8dbf85ce34992a85b19cdbf
lucky verma <thinkersloop@gmail.com>
2022-04-23 06:49:35 -0400

    min cut plot

--- README.md
+++ README.md
@@ -1,4 +1,46 @@
-# DAA-Project1-Spring22
+# DAA_Project_1_Spring22
 
-`MK97893, Lucky Verma`
+>  `MK97893, Lucky Verma`
 
+## Installation [GitHub Pages](https://pages.github.com/)
+
+```
+>>> pip install -r requirements.txt
+>>> python setup.py install 
+```
+
+## Pymaxflow Usage
+
+The method `add_grid_nodes` adds multiple nodes and returns their indices in a convenient n-dimensional array with the given shape; `add_grid_edges` adds edges to the grid with a given neighborhood structure (4-connected by default); and `add_grid_tedges` sets the capacities of the terminal edges for multiple nodes.
+    
+```python
+import pymaxflow as mf
+
+# Create a grid of nodes
+nodes = mf.add_grid_nodes(shape=(10, 10))
+
+# Add edges to the grid
+mf.add_grid_edges(nodes, neighborhood=mf.neighborhoods.four_connected)
+
+# Set the capacities of the terminal edges
+mf.add_grid_tedges(nodes, capacities=[1, 1, 1, 1])
+
+# Set the demands of the nodes
+mf.set_demands(nodes, demands=[1, 1, 1, 1])
+```
+
+## Usage
+- Testing images can be found in the `test` folder.
+
+``` python
+image_dir = r"{}/test".format(os.getcwd())
+```
+
+## TODO
+
+- [x] cut of the graph into subgraphs and merge them into one. :shipit:
+- [x] Get the segments of the nodes in the grid.
+- [ ] couple of intermediate outputs
+- [ ] Image showing clear overlapped region # Important
+- [x] Min cut over images # Important
+- [ ] Try **setuptools** to package the project.
---
+++ main.py
@@ -0,0 +1,118 @@
+import sys
+import cv2
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import os
+import maxflow
+import argparse
+from matplotlib.pyplot import figure
+
+image_dir = r"{}/test".format(os.getcwd())
+src = cv2.imread(os.path.join(image_dir, 'src.jpg'))
+sink = cv2.imread(os.path.join(image_dir, 'target.jpg'))
+mask = cv2.imread(os.path.join(image_dir, 'mask.png'))
+
+def compute_edge_weights(src, sink):
+    """
+    Computes edge weights based on matching quality cost.
+    :param src: image to be blended (foreground)
+    :param sink: background image
+    """
+    edge_weights = np.zeros((src.shape[0], src.shape[1], 2))
+
+    # Create shifted versions of the matrices for vectorized operations.
+    src_left_shifted = np.roll(src, -1, axis=1)
+    sink_left_shifted = np.roll(sink, -1, axis=1)
+    src_up_shifted = np.roll(src, -1, axis=0)
+    sink_up_shifted = np.roll(sink, -1, axis=0)
+
+    # Assign edge weights.
+    # For numerical stability, avoid divide by 0.
+    eps = 1e-10
+
+    # Right neighbor.
+    weight = np.sum(np.square(src - sink, dtype=np.float) +
+                    np.square(src_left_shifted - sink_left_shifted, 
+                    dtype=np.float),
+                    axis=2)
+    norm_factor = np.sum(np.square(src - src_left_shifted, dtype=np.float) +
+                         np.square(sink - sink_left_shifted, 
+                         dtype=np.float),
+                         axis=2)
+    edge_weights[:, :, 0] = weight / (norm_factor + eps)
+
+    # Bottom neighbor.
+    weight = np.sum(np.square(src - sink, dtype=np.float) +
+                    np.square(src_up_shifted - sink_up_shifted,
+                    dtype=np.float),
+                    axis=2)
+    norm_factor = np.sum(np.square(src - src_up_shifted, dtype=np.float) +
+                         np.square(sink - sink_up_shifted, 
+                         dtype=np.float),
+                         axis=2)
+    edge_weights[:, :, 1] = weight / (norm_factor + eps)
+    
+    return edge_weights
+
+assert (src.shape == sink.shape), f"Source and sink dimensions must be the same: {str(src.shape)} != {str(sink.shape)}"
+
+
+if __name__ == '__main__':
+    # Create the graph
+    graph = maxflow.Graph[float]()
+    # Add the nodes. node_ids has the identifiers of the nodes in the grid.
+    node_ids = graph.add_grid_nodes((src.shape[0], src.shape[1]))
+
+    edge_weights = compute_edge_weights(src, sink)
+
+    # Add non-terminal edges
+    patch_height = src.shape[0]
+    patch_width = src.shape[1]
+    for row_idx in range(patch_height):
+        for col_idx in range(patch_width):
+            # right neighbor
+            if col_idx + 1 < patch_width:
+                weight = edge_weights[row_idx, col_idx, 0]
+                graph.add_edge(node_ids[row_idx][col_idx],
+                            node_ids[row_idx][col_idx + 1],
+                            weight,
+                            weight)
+
+            # bottom neighbor
+            if row_idx + 1 < patch_height:
+                weight = edge_weights[row_idx, col_idx, 1]
+                graph.add_edge(node_ids[row_idx][col_idx],
+                            node_ids[row_idx + 1][col_idx],
+                            weight,
+                            weight)
+
+            # Add terminal edge capacities for the pixels constrained to
+            # belong to the source/sink.
+            if np.array_equal(mask[row_idx, col_idx, :], [0, 255, 255]):
+                graph.add_tedge(node_ids[row_idx][col_idx], 0, np.inf)
+            elif np.array_equal(mask[row_idx, col_idx, :], [255, 128, 0]):
+                graph.add_tedge(node_ids[row_idx][col_idx], np.inf, 0)
+
+    sgm = graph.get_grid_segments(node_ids)
+
+    sink[sgm] = src[sgm]
+    rgb_img = cv2.cvtColor(src , cv2.COLOR_BGR2RGB)
+    plt.imshow(rgb_img)
+    plt.savefig(os.path.join(image_dir, 'result.png'))
+    plt.show()
+
+    # # Compute intermediate result
+    # canvas = np.zeros(src.shape, dtype=np.uint8)
+    
+    # # Fill in the pixels constrained to belong to the src.
+    # src[sgm] = canvas[sgm]
+    # plt.savefig(os.path.join(image_dir, "result_src.png"))
+    # plt.show()
+
+    # # Fill in the pixels constrained to belong to the sink.
+    # sink[sgm] = canvas[sgm] 
+    # plt.savefig(os.path.join(image_dir, "result_sink.png"))
+    # plt.show()
+    sys.exit()
+
